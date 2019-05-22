0dc9f73954e3b74c1922a8873dba2544d9114e57
Michael Bravo <michael.bravo777@gmail.com>
2019-05-22 16:29:34 -0400

    finished accordion componenet

--- src/index.js
+++ src/index.js
@@ -2,17 +2,32 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import HelloWorld from './HelloWorld';
-import RouletteGun from './RouletteGun';
+import Accordion from './state-drills/Accordion';
+// import RouletteGun from './RouletteGun';
 import * as serviceWorker from './serviceWorker';
 // removed import App 
 
 
 // HelloWorld = new HelloWorld();
 
+const sections = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
+// e.g. <Accordion sections={sections} />
 
-
-
-ReactDOM.render(<RouletteGun randomNumber={8} />, document.getElementById('root'));
+// ReactDOM.render(<RouletteGun randomNumber={8} />, document.getElementById('root'));
+ReactDOM.render(<Accordion sections={sections} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Accordion extends React.Component {
+
+  state = {
+    currentSelection: -1
+  }
+
+  handleClick = (index) => {
+    this.setState( { 
+      currentSelection: index
+    } );
+
+  }
+
+
+  render() {
+    return ( 
+      <div>
+        <ul>
+            {this.props.sections.map((item, index) => {
+              return <li key={index}>
+                <button onClick={() => this.handleClick(index)}>{item.title}</button>
+                {(this.state.currentSelection === index) && (<p>{item.content}</p>)}
+              </li>
+            })}
+        </ul>
+      </div>
+    )
+
+    
+  }
+
+}
+
+export default Accordion;
